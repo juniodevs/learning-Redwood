4bc64d1daf096ab291c4139a208d97bd280a55f4
Júnior Veras <iramarjunhorveras@gmail.com>
2023-05-20 02:13:35 -0300

    Cap 3/4

---
+++ my-redwood-project/web/src/pages/ArticlePage/ArticlePage.js
@@ -0,0 +1,13 @@
+  import { MetaTags } from '@redwoodjs/web'
+  import ArticlesCell from 'src/components/ArticlesCell'
+
+  const ArticlePage = () => {
+    return (
+      <>
+        <MetaTags title="Article" description="Article page" />
+        <ArticlesCell />
+      </>
+    )
+  }
+
+  export default ArticlePage
